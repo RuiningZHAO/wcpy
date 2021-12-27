52c4d8f2db9ede9c8eb4f17ed33a148ce79c7a99
Ruining ZHAO <rzhao@nao.cas.cn>
2021-12-28 01:23:02 +0800

    Update refinePeaks

--- utils.py
+++ utils.py
@@ -90,34 +90,31 @@ def saveSpectrum(spectrum, peak_prop, header, filename, saveECSV):
     if saveECSV:
         spectrum.write(os.path.splitext(filename)[0] + '_spec.ecsv', format='ascii.ecsv', overwrite=True)
         peak_prop.write(os.path.splitext(filename)[0] + '_peak.ecsv', format='ascii.ecsv', overwrite=True)
-    
 
 def Gaussian(x, a, x0, sigma):
-    """1D Gaussian
+    """1D Gaussian function.
 
     Parameters
     ----------
     x: float or `numpy.ndarray`
-        value or values to evaluate the Gaussian at
+        Value or values to evaluate the Gaussian at.
     a: float
-        Magnitude
+        Magnitude.
     x0: float
-        Gaussian centre
+        Gaussian centre.
     sigma: float
-        Standard deviation (spread)
+        Standard deviation.
 
     Returns
     -------
     out: float or `numpy.ndarray`
-        The Gaussian function evaluated at provided x
+        The Gaussian function evaluated at provided x.
     """
-    
-    # return a * np.exp(-(x - x0)**2 / (2 * sigma**2 + 1e-9))
+
     return a * np.exp(-(x - x0)**2 / (2 * sigma**2))
 
-def refine_peaks(spectrum, peaks, properties):
-    """
-    Refine peak locations in a spectrum from a set of initial estimates.
+def refinePeaks(spectrum, peaks, properties):
+    """Refine peak locations in a spectrum from a set of initial estimates.
     This function attempts to fit a Gaussian to each peak in the provided
     list. It returns a list of sub-pixel refined peaks. If two peaks are
     very close, they can be refined to the same location. In this case
@@ -127,9 +124,9 @@ def refine_peaks(spectrum, peaks, properties):
     Parameters
     ----------
     spectrum: array_like
-        Input spectrum (intensities)
+        Input spectrum (intensities).
     peaks: array_like
-        Peak locations in pixels
+        Peak locations in pixels.
     properties: dict
         Peak properties dict returned by `scipy.signal.find_peaks`. Should
         contain `peak_heights`, `left_bases` and `right_bases`.
@@ -137,25 +134,37 @@ def refine_peaks(spectrum, peaks, properties):
     Returns
     -------
     refined_peaks: `numpy.ndarray`
-        Refined peak locations
+        Refined peak locations in pixels.
     """
 
-    refined_peaks = list()
-    mask = list()
+    # Validate inputs
+    try:
+        spectrum = np.array(spectrum)
+    except:
+        raise ValueError(f'Illegal type {type(spectrum)} for refining peak locations.')
 
-    spectrum = np.array(spectrum)
-    length = spectrum.shape[0]
-    index = np.arange(length)
+    try:
+        peaks = np.array(peaks)
+    except:
+        raise ValueError(f'Illegal type {type(peaks)} for refining peak locations.')
+    
+    if ('peak_heights' not in properties) | \
+       ('left_bases' not in properties) | \
+       ('right_bases' not in properties):
+        raise ValueError('``properties`` should contain `peak_heights`, `left_bases` and `right_bases`.')
 
-    peaks = np.round(peaks, 0).astype(int)
-    npeaks = peaks.shape[0]
+    index = np.arange(spectrum.shape[0])
+
+    n_peaks = peaks.shape[0]
 
     heights = properties['peak_heights']
     left_bases = properties['left_bases']
     right_bases = properties['right_bases']
     widths = right_bases - left_bases
 
-    for i in range(npeaks):
+    refined_index = list()
+    refined_peaks = list()
+    for i in range(n_peaks):
         # Refine left base
         if i == 0:
             left_base = left_bases[i]
@@ -164,23 +173,24 @@ def refine_peaks(spectrum, peaks, properties):
         else:
             left_base = np.max([left_bases[i], right_bases[i - 1]])
         # Refine right base
-        if i == (npeaks - 1):
+        if i == (n_peaks - 1):
             right_base = right_bases[i]
         elif left_bases[i + 1] <= peaks[i]:
             right_base = right_bases[i]
         else:
             right_base = np.min([right_bases[i], left_bases[i + 1]])
-        
+        # too few points
         if (right_base + 1 - left_base) < 5:
-            left_base = peaks[i] - 2
-            right_base = peaks[i] + 2
+            left_base = int(peaks[i]) - 2
+            right_base = int(peaks[i]) + 2
             if left_base < 0:
-                right_base += 0 - left_base
+                right_base -= left_base
                 left_base = 0
-            if right_base > (length - 1):
-                left_base -= (right_base - length + 1)
-                right_base = length - 1
-
+            if right_base > index[-1]:
+                left_base -= (right_base - index[-1])
+                right_base = index[-1]
+        
+        # Fit
         x = index[left_base:(right_base + 1)]
         y = spectrum[left_base:(right_base + 1)]
 
@@ -189,29 +199,28 @@ def refine_peaks(spectrum, peaks, properties):
             height, centre, _ = popt
 
             if (height > 0) & \
-               (0 < centre) & \
-               (centre < (length - 1)):
+               (left_base < centre) & \
+               (centre < right_base):
+                refined_index.append(i)
                 refined_peaks.append(centre)
-                mask.append(True)
-            else:
-                mask.append(False)
-                continue
 
         except RuntimeError:
-            mask.append(False)
             continue
+
+    refined_index = np.array(refined_index)
     refined_peaks = np.array(refined_peaks)
 
-    # Remove peaks that are within rounding errors from each other from the curve_fit
-    distance_mask = np.isclose(refined_peaks[:-1], refined_peaks[1:])
-    distance_mask = np.hstack([False, distance_mask])
+    # Mask peaks that are within rounding errors from each other
+    mask = np.isclose(refined_peaks[:-1], refined_peaks[1:])
+    mask = np.hstack([False, mask])
 
-    refined_peaks = refined_peaks[~distance_mask]
+    refined_index = refined_index[~mask]
+    refined_peaks = refined_peaks[~mask]
     refined_properties = dict()
     for key, val in properties.items():
-        refined_properties[key] = val[mask][~distance_mask]
+        refined_properties[key] = val[refined_index]
 
-    return refined_peaks, refined_properties
+    return refined_peaks, refined_properties, refined_index
 
 def findPeaks(x, **kwargs):
     """
@@ -221,9 +230,10 @@ def findPeaks(x, **kwargs):
     peaks, properties = find_peaks(x=x, **kwargs)
     if peaks.shape[0] > 0:
         # Refine peaks
-        return refine_peaks(spectrum=x, 
-                            peaks=peaks, 
-                            properties=properties)
+        refined_peaks, refined_properties, _ = refinePeaks(spectrum=x, 
+                                                           peaks=peaks, 
+                                                           properties=properties)
+        return refined_peaks, refined_properties
     else:
         return peaks, properties
 
